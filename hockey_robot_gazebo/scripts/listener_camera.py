ec49808a70478393426facc50fb625c76c9b481d
Xu Mingjie <xumj1996@gmail.com>
2022-03-02 01:00:21 +0800

    camera listener py

---
+++ hockey_robot_gazebo/scripts/listener_camera.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2008, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Revision $Id$
+
+## Simple talker demo that listens to std_msgs/Strings published 
+## to the 'chatter' topic
+
+import numpy
+from sympy import im
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+import cv2
+
+def create_blob_detector(roi_size=(128, 128), blob_min_area=3, 
+                         blob_min_int=.5, blob_max_int=.95, blob_th_step=10):
+    params = cv2.SimpleBlobDetector_Params()
+    params.filterByArea = True
+    params.minArea = blob_min_area
+    params.maxArea = roi_size[0]*roi_size[1]
+    params.filterByCircularity = False
+    params.filterByColor = False
+    params.filterByConvexity = False
+    params.filterByInertia = False
+    # blob detection only works with "uint8" images.
+    params.minThreshold = int(blob_min_int*255)
+    params.maxThreshold = int(blob_max_int*255)
+    params.thresholdStep = blob_th_step
+    ver = (cv2.__version__).split('.')
+    if int(ver[0]) < 3:
+        return cv2.SimpleBlobDetector(params)
+    else:
+        return cv2.SimpleBlobDetector_create(params)
+
+def hockey_detection(img:numpy.ndarray,roi_size=(128, 128),blob_min_area=3):
+    params = cv2.SimpleBlobDetector_Params()
+    params.filterByArea = True
+    params.minArea = blob_min_area
+    params.maxArea = roi_size[0]*roi_size[1]
+
+def callback(data : Image):
+    # print(data.header)
+    if data.header.seq % 300 == 0:
+        img = bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
+        print (type(img))
+        # filename = '{}{}.jpg'.format(save_dir,data.header.seq)
+        # cv2.imwrite(filename, img)
+    
+    # rospy.loginfo(rospy.get_caller_id() + 'position: %s', data.position)
+
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('listener', anonymous=True)
+
+    rospy.Subscriber('/hockey_robot/camera1/image_raw', Image, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    # f = open("demofile3.txt", "w")
+    save_dir = '/home/xumingjie/catkin_ws/src/air_hockey_robot/hockey_robot_gazebo/scripts/'
+    bridge = CvBridge()
+    listener()
+    # f.close()
